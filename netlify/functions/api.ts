26addc2bd9603a62b38f87640a85f5335594a464
pavelbeard <pavelborodin1995@gmail.com>
2025-07-19 16:49:07 +0200

    Implement Express server with API routes and hello world response

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,4 +1,21 @@
 import serverless from "serverless-http";
-import app from "../../apps/api/index";
+
+import express from "express";
+import router from "./routes";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("/api/hello", router);
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
 
 export const handler = serverless(app);
